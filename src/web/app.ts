c577299c172650c9b766652955fffbed3f2ba0f5
Navneet Patel <navneetp22875@gmail.com>
2026-09-18 11:39:25 +0530

    refactor: remove deployment changes message from health check response

--- src/web/app.ts
+++ src/web/app.ts
@@ -31,7 +31,6 @@ async function health(_req: express.Request, res: express.Response) {
   try {
     await sequelize.authenticate();
     res.json({
-      changes: 'auto deployment changes testing',
       status: 'ok',
       service: 'budgetbrain-web-api',
       version: env.API_VERSION,
